9b5ceb8ba19b05f9677472145553f5eee2fe6019
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-05-01 16:26:12 -0400

    removed unneeded stuff

--- analysis/neutron/helpers.py
+++ analysis/neutron/helpers.py
@@ -26,34 +26,3 @@ def get_exp_cllif_density(temp, LiCl_frac=0.695):
     rho = a + b * C + c * temp + d * C**2 + e * C**3 + f * temp * C**2 + g * C * temp**2
 
     return rho
-
-
-def calculate_cylinder_volume(radius, height):
-    volume = math.pi * radius**2 * height
-    return volume
-
-
-def translate_surface(
-    surface: (
-        openmc.XPlane | openmc.YPlane | openmc.ZPlane | openmc.Plane | openmc.Sphere
-    ),
-    dx: float,
-    dy: float,
-    dz: float,
-) -> openmc.Surface:
-    """Translate an OpenMC surface by dx, dy, dz."""
-    if isinstance(surface, openmc.XPlane):
-        surface.x0 += dx
-    elif isinstance(surface, openmc.YPlane):
-        surface.y0 += dy
-    elif isinstance(surface, openmc.ZPlane):
-        surface.z0 += dz
-    elif isinstance(surface, openmc.Plane):
-        surface.d += surface.a * dx + surface.b * dy + surface.c * dz
-    elif isinstance(surface, openmc.Sphere):
-        surface.x0 += dx
-        surface.y0 += dy
-        surface.z0 += dz
-    else:
-        raise TypeError(f"Unsupported surface type: {type(surface)}")
-    return surface
